37c6899f82f605ade733b0e96df94e00686d7bdc
jeonghun10 <jeonghun8910@gmail.com>
2022-03-08 11:08:44 +0900

    Fix : 코드정리

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import theme from '../styles/theme';
 
 export default function SelectBox({ name, data }) {
-  console.log(data);
   return (
     <SelectWrapStyled>
       <label>{name}</label>
