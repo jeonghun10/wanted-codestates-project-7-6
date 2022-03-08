67a12548740488dccc072f160883e2c7e6aba086
jeonghun10 <jeonghun8910@gmail.com>
2022-03-08 09:30:03 +0900

    Fix : selectBox 컴포넌트 추가 및 pr리뷰 수정사항 반영

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,100 +1,98 @@
 import styled from 'styled-components';
 import theme from '../styles/theme';
 import { timeList, careTimeList } from '../temp/staticData';
+import SelectBox from '../components/SelectBox';
 
 export default function Schedule() {
   return (
-    <ContainerStyled>
-      <h2>돌봄 스케줄을 설정해주세요</h2>
-      <InputWrapStyled>
-        <CalendarWrapStyled>
-          <span>시작일</span>
+    <Container>
+      <CalendarWrap>
+        <CalendarSection>
+          <CalendarSpan>시작일</CalendarSpan>
           <CalendarButton>
-            <span>월 일</span>
+            <span>날짜 선택</span>
             <span>아이콘</span>
           </CalendarButton>
-        </CalendarWrapStyled>
-        <CalendarWrapStyled>
-          <span>종료일</span>
+        </CalendarSection>
+        <CalendarSection>
+          <CalendarSpan>종료일</CalendarSpan>
           <CalendarButton>
-            <span>월 일</span>
+            <span>날짜 선택</span>
             <span>아이콘</span>
           </CalendarButton>
-        </CalendarWrapStyled>
-      </InputWrapStyled>
-      <SelectWrapStyled>
-        <span>돌봄 시작 시간</span>
-        <select name="돌봄 시작 시간">
-          {timeList.map((el, idx) => (
-            <option key={idx}>{el.text}</option>
-          ))}
-        </select>
-      </SelectWrapStyled>
-      <SelectWrapStyled>
-        <span>하루 돌봄 시간</span>
-        <select name="하루 돌봄 시간">
-          {careTimeList.map((el, idx) => (
-            <option key={idx}>{el.text}</option>
-          ))}
-        </select>
-      </SelectWrapStyled>
-      <NoticeStyled>
+        </CalendarSection>
+      </CalendarWrap>
+      <SelectBox name="하루 돌봄 시간" data={timeList} />
+      <SelectBox name="돌봄 시작 시간" data={careTimeList} />
+      <Notice>
         <p>하루 기본 3시간에서 12시간까지 선택가능합니다.</p>
-      </NoticeStyled>
-    </ContainerStyled>
+      </Notice>
+    </Container>
   );
 }
 
-const ContainerStyled = styled.div`
+const Container = styled.div`
   width: 768px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 32px 16px 64px;
   margin: 0 auto;
-  color: ${theme.fontColor};
-  font-weight: 600;
-  letter-spacing: -1px;
-  h2 {
-    margin-bottom: 4em;
-  }
+  position: static;
+  left: 0px;
+  right: 0px;
+  top: 0px;
+  overflow-y: scroll;
+  color: ${({ theme }) => theme.fontColor};
 `;
-const InputWrapStyled = styled.div`
+const CalendarWrap = styled.div`
   display: flex;
+  flex-direction: row;
 `;
-const CalendarWrapStyled = styled.div`
-  width: 50%;
-  margin-bottom: 4em;
+const CalendarSection = styled.div`
+  display: flex;
+  flex-direction: column;
   &: first-child {
-    margin-right: 20px;
+    margin-right: 8px;
   }
 `;
+
+const CalendarSpan = styled.span`
+  flex: none;
+  position: static;
+  width: 160px;
+  height: 20px;
+  left: 0px;
+  top: 0px;
+  font-weight: bold;
+`;
+
 const CalendarButton = styled.button`
-  backgound: ${theme.inputGray};
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  padding: 15px;
+  flex: none;
+  width: 160px;
+  height: 48px;
+  position: relative;
+  left: 0%;
+  right: 0%;
+  top: 0%;
+  bottom: 0%;
+  background: ${({ theme }) => theme.inputGray.backgroundColor};
+  border-radius: 4px;
   border: 0;
   outline: 0;
+  margin-bottom: 16px;
+  span {
+    color: ${({ theme }) => theme.inputBorder.placeholderTextColor};
+    width: 112px;
+    heihgt: 20px;
+    justify-content: space-between;
+  }
   &:hover {
-    outline: 2px solid ${theme.fontColor};
-    cursor: pointer;
+    outline: 1px solid #000000;
   }
 `;
-const SelectWrapStyled = styled.div`
-  margin-bottom: 4em;
-  select {
-    display: block;
-    width: 100%;
-    padding: 0.5em 0.7em;
-    font-size: 1rem;
-    font-weight: 400;
-    color: ${theme.fontColor};
-    border: 1px solid ${theme.inputGray};
-    border-radius: 0.25rem;
-  }
-  select:focus {
-    outline: 1px solid ${theme.fontColor};
-  }
-`;
-const NoticeStyled = styled.div`
+
+const Notice = styled.div`
   background: rgb(244, 247, 255);
   padding: 1em;
   color: rgb(20, 27, 77);
